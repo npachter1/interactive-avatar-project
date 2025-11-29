26a4d0ebc86cb5f9ff902081573f1587bb167be1
npachter1 <neil_pachter@comcast.net>
2025-11-28 19:39:07 -0700

    remove reference to heygen in page title

--- avatar-orchestrator/app/layout.tsx
+++ avatar-orchestrator/app/layout.tsx
@@ -16,11 +16,11 @@ const fontMono = FontMono({
 
 export const metadata: Metadata = {
   title: {
-    default: "HeyGen Interactive Avatar SDK Demo",
+    default: "Interactive AI Avatar SDK Demo",
     template: `%s - HeyGen Interactive Avatar SDK Demo`,
   },
   icons: {
-    icon: "/heygen-logo.png",
+    // icon: "/heygen-logo.png",
   },
 };
 
